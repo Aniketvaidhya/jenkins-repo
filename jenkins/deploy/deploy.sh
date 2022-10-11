38c975692b56367909bf8f5cae5c1335c58a0902
Ubuntu <ubuntu@ip-172-31-24-199.ap-northeast-1.compute.internal>
2022-10-11 23:42:43 +0000

    add scp

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,5 +3,5 @@
 echo $BUILD_TAG > /tmp/.auth
 echo $DOCKER_PW >> /tmp/.auth
 
-scp -i ~/.ssh/id_rsa ubuntu@13.230.95.37:/tmp/.auth
+scp -i ~/.ssh/id_rsa /tmp/.auth  ubuntu@13.230.95.37:/tmp/.auth
 ssh -i ~/.ssh/id_rsa ubuntu@13.230.95.37 "/home/ubuntu/maven-app/deploy.sh"
