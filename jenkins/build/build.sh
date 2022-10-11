4962d4ab8ea6faadf796886583cd05ca5bad26c1
Ubuntu <ubuntu@ip-172-31-24-199.ap-northeast-1.compute.internal>
2022-10-11 07:17:27 +0000

    copy target/*.jar to build directory

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -5,7 +5,7 @@ echo "###### Building App ##################"
 echo "######################################"
 
 # Copy *.jar from java-app/targets/*jar. to openjdk container
-#cp ~/pipeline/java-app/target/*.jar ~/pipeline/jenkins/build/
+cp ./java-app/target/*.jar ./jenkins/build/
 
 #cd jenkins/build/ && 
 docker-compose -f ./jenkins/build/docker-compose-build.yaml build --no-cache
