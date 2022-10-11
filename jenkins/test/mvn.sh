3aef03248f864999d1c07c08009f71dc883190a1
Ubuntu <ubuntu@ip-172-31-24-199.ap-northeast-1.compute.internal>
2022-10-11 09:16:22 +0000

    revise WORKSPACE path

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,7 +4,7 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
-WORKSPACE="jenkins/jenkins_home/workspace/maven-app"
+WORKSPACE="/home/ubuntu/pipeline/jenkins/jenkins_home/workspace/maven-app"
 
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3.5.4 "$@"
 
