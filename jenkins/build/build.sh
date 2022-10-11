3f108be082986328f7c7526c6480ca15fca11f05
Ubuntu <ubuntu@ip-172-31-24-199.ap-northeast-1.compute.internal>
2022-10-11 09:21:30 +0000

    modify *.jar path

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -5,7 +5,7 @@ echo "###### Building App ##################"
 echo "######################################"
 
 # Copy *.jar from java-app/targets/*jar. to openjdk container
-cp *.jar jenkins/build/
+cp java-app/target/*.jar jenkins/build/
 
 #cd jenkins/build/ && 
 docker-compose -f docker-compose-build.yaml build --no-cache
