56297f6aca5f5cab8d21c4f9a8e4801c78166d13
Ubuntu <ubuntu@ip-172-31-24-199.ap-northeast-1.compute.internal>
2022-10-11 07:01:09 +0000

    revise build script

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -5,6 +5,7 @@ echo "###### Building App ##################"
 echo "######################################"
 
 # Copy *.jar from java-app/targets/*jar. to openjdk container
-cp ~/pipeline/java-app/target/*.jar ~/pipeline/jenkins/build/
+#cp ~/pipeline/java-app/target/*.jar ~/pipeline/jenkins/build/
 
-cd jenkins/build/ && docker-compose -f docker-compose-build.yaml build --no-cache
+#cd jenkins/build/ && 
+docker-compose -f docker-compose-build.yaml build --no-cache
