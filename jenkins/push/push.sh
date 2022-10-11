b36ed3364f78de241f78adbb0423443ef6e61bb2
Ubuntu <ubuntu@ip-172-31-24-199.ap-northeast-1.compute.internal>
2022-10-11 09:25:43 +0000

    eliminate the space between -p and

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -4,6 +4,6 @@ echo "################################"
 echo "### Push image to Docker Hub ###"
 echo "################################"
 
-docker login -u $DOCKER_ID -p $DOCKER_PW
+docker login -u $DOCKER_ID -p$DOCKER_PW
 
 docker push hsinyih/java-app:v$BUILD_TAG
