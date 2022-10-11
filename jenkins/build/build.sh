5f857691e7a61342bf42fc841a93f9b80baeebf6
Ubuntu <ubuntu@ip-172-31-24-199.ap-northeast-1.compute.internal>
2022-10-11 07:36:57 +0000

    revise build

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -8,4 +8,4 @@ echo "######################################"
 cp ./java-app/target/*.jar ./jenkins/build/
 
 #cd jenkins/build/ && 
-docker-compose -f ./jenkins/build/docker-compose-build.yaml build --no-cache
+docker-compose -f ./docker-compose-build.yaml build --no-cache
