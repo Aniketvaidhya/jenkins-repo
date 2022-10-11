048d239c42ec88eb3b7faa098bca999d64f80eac
Ubuntu <ubuntu@ip-172-31-24-199.ap-northeast-1.compute.internal>
2022-10-11 21:57:47 +0000

    scp variable values to remote host

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#export BUILD_TAG=$(sed -n "1p" /tmp/.auth)
-#export DOCKER_ID=$(sed -n "2p" /tmp/.auth)
-#export DOCKER_PW=$(sed -n "3p" /tmp/.auth)
+echo $BUILD_TAG > /tmp/.auth
+echo $DOCKER_PW >> /tmp/.auth
 
+scp -i ~/.ssh/id_rsa ubuntu@13.230.95.37:/tmp/.auth
 docker login -u hsinyih -p$DOCKER_PW
 ssh -i ~/.ssh/id_rsa ubuntu@13.230.95.37 "cd /home/ubuntu/maven-app && docker-compose up -d"
