8d394d2e39f364280b7d13f2432b5e850a4b09db
Ubuntu <ubuntu@ip-172-31-24-199.ap-northeast-1.compute.internal>
2022-10-11 22:26:09 +0000

    revise script

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,5 +4,4 @@ echo $BUILD_TAG > /tmp/.auth
 echo $DOCKER_PW >> /tmp/.auth
 
 scp -i ~/.ssh/id_rsa ubuntu@13.230.95.37:/tmp/.auth
-docker login -u hsinyih -p$DOCKER_PW
-ssh -i ~/.ssh/id_rsa ubuntu@13.230.95.37 "cd /home/ubuntu/maven-app && docker-compose up -d"
+ssh -i ~/.ssh/id_rsa ubuntu@13.230.95.37 "/home/ubuntu/maven-app/deploy.sh"
