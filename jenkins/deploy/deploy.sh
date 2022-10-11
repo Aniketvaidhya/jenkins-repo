c61c6b77146189d603a101ac2b4c2b8979c3fb13
Ubuntu <ubuntu@ip-172-31-24-199.ap-northeast-1.compute.internal>
2022-10-11 21:51:36 +0000

    add ssh command

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export BUILD_TAG=$(sed -n "1p" /tmp/.auth)
-export DOCKER_ID=$(sed -n "2p" /tmp/.auth)
-export DOCKER_PW=$(sed -n "3p" /tmp/.auth)
-docker login -u $DOCKER_ID -p$DOCKER_PW
-cd /home/ubuntu/maven-app && docker-compose up -d
+#export BUILD_TAG=$(sed -n "1p" /tmp/.auth)
+#export DOCKER_ID=$(sed -n "2p" /tmp/.auth)
+#export DOCKER_PW=$(sed -n "3p" /tmp/.auth)
+
+docker login -u hsinyih -p$DOCKER_PW
+ssh -i ~/.ssh/id_rsa ubuntu@13.230.95.37 "cd /home/ubuntu/maven-app && docker-compose up -d"
